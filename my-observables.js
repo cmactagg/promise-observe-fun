01f8e99812c8ece2b20e243035a5f09ff4b080f9
mtags <mtags22@gmail.com>
2016-05-30 14:38:15 -0600

    play with rx subject

--- my-observables.js
+++ my-observables.js
@@ -11,7 +11,23 @@ function MyObservables() {
     return Rx.Observable.create(function(observer) {
       console.log("inside runObservable");
       observer.next("it worked");
+      
+      observer.complete();
     });
+  }
+  
+  this.deferObs = function(){
+    return Rx.Observable.defer(this.runObservable());
+  }
+  
+  var someval = "sadie"
+  
+  this.ofObs = function(){
+    return Rx.Observable.of(someval, 2, 3, 4, 5);
+  }
+  
+  this.seqStuff = function(){
+    return seq([,,,,1,,,,2,,,,3]);
   }
 
 }
--- server.js
+++ server.js
@@ -15,7 +15,33 @@ var Rx = require('rxjs/RX');
 //   console.log("run promise reject");
 // });
 
-myObservables.runObservable().subscribe(value => console.log(value));
+// myObservables.runObservable().subscribe(value => console.log(value), error => console.log(error.message), complete => console.log("done"));
+
+// myObservables.runObservable().subscribe(value => console.log(value), error => console.log(error.message), complete => console.log("done1"));
+
+// myObservables.deferObs().subscribe(value => console.log(value), error => console.log(error.message), complete => console.log("done2"));
+
+// myObservables.deferObs().subscribe(value => console.log(value), error => console.log(error.message), complete => console.log("done3"));
+
+// myObservables.seqStuff().subscribe(
+//   val => console.log(val),
+//   err => console.log(err.message),
+//   () => console.log("done")
+// );
+
+
+
+var subject = new Rx.BehaviorSubject();
+
+subject.next('old value');
+subject.next('before subscription');
+
+subject.subscribe(function(val) {
+  console.log(val);
+});
+
+subject.next('after subscription');
+
 
 
 console.log("END");
